3e36605cc522d831031a9cfc6c6c94f388c3536c
qwerty <bill4678key@gmail.com>
2019-03-09 09:35:07 +0200

    smth

--- WindowsFormsApp3/Dbclass.cs
+++ WindowsFormsApp3/Dbclass.cs
@@ -10,7 +10,7 @@ namespace WindowsFormsApp3
 {
     class Dbclass
     {
-        string connectString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\WindowsFormsApp3\\WindowsFormsApp3\\bin\\Debug\\projectNo1.mdf;Integrated Security=True;Connect Timeout=30";
+        string connectString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\qwerty\\source\\repos\\WindowsFormsApp3\\WindowsFormsApp3\\projectNo1.mdf;Integrated Security=True;Connect Timeout=30";
         public void connect2Db(string query)
         {
 
