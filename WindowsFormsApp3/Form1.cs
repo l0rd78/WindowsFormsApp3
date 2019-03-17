676d79ad27a60cf6c85b9591c19a1fb9f8673360
qwerty <bill4678key@gmail.com>
2019-03-17 10:07:06 +0200

    problems resolved

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -126,19 +126,6 @@ namespace WindowsFormsApp3
             sfd.FileName = "Inventory_Adjustment_Export.xls";
             if (sfd.ShowDialog() == DialogResult.OK)
             {
-                copyAlltoClipboard();
-                Microsoft.Office.Interop.Excel.Application xlexcel;
-                Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
-                Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
-                object misValue = System.Reflection.Missing.Value;
-                xlexcel = new Microsoft.Office.Interop.Excel.Application();
-                xlexcel.Visible = true;
-                xlWorkBook = xlexcel.Workbooks.Add(misValue);
-                xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-                Microsoft.Office.Interop.Excel.Range CR = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[1, 1];
-                CR.Select();
-                xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);          
-                /*
 
                 // Create an instance of the class that exports Excel files, having one sheet
                 ExcelDocument workbook = new ExcelDocument(1);
